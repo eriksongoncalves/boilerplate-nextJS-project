5b7b471d789520a008ebebfc2144c8784ddb890d
Erikson Gonçalves <erikson.bezerra@amedigital.com>
2021-05-07 13:10:57 -0300

    chore: update libs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,11 +9,11 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
       <Head>
-        <title>Won Games</title>
+        <title>Boilerplate - NextJS</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
-        <meta name="description" content="The best Game Stores in the world!" />
+        <meta name="description" content="" />
       </Head>
       <GlobalStyles />
       <Component {...pageProps} />
