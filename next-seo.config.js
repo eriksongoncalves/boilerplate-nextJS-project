bc014369409b95f077b9e4e1ba7c1564b5d1dd5a
Erikson Gonçalves <erikson.bezerra@amedigital.com>
2021-09-02 17:54:54 -0300

    chore: update libs

---
+++ next-seo.config.js
@@ -0,0 +1,9 @@
+export default {
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: 'https://www.dominio.com.br',
+    site_name: 'Site name',
+    title: 'Boilerplate - NextJS'
+  }
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,11 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import { ThemeProvider } from 'styled-components';
+import { DefaultSeo } from 'next-seo';
 
 import GlobalStyles from 'styles/global';
 import theme from 'styles/theme';
+import SEO from '../../next-seo.config';
 
 function App({ Component, pageProps }: AppProps) {
   return (
@@ -15,6 +17,7 @@ function App({ Component, pageProps }: AppProps) {
         <link rel="manifest" href="/manifest.json" />
         <meta name="description" content="" />
       </Head>
+      <DefaultSeo {...SEO} />
       <GlobalStyles />
       <Component {...pageProps} />
     </ThemeProvider>
